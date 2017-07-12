5447714e2d5b5561ac271a422b896abc32acf831
Vertaler <dancheran@yandex.ru>
2017-07-12 18:12:35 +0300

    main window

---
+++ mainwindow.cpp
@@ -0,0 +1,23 @@
+#include "mainwindow.h"
+#include "testwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::slotStartTestClicked(){
+    TestWindow* tw = new TestWindow();
+    //tw->setWindowState( Qt::WindowFullScreen);
+    tw->showFullScreen();
+}
---
+++ test.pro
@@ -0,0 +1,24 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2017-07-11T19:16:01
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = test
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp \
+    testwindow.cpp
+
+HEADERS  += mainwindow.h \
+    testwindow.h
+
+FORMS    += mainwindow.ui \
+    testwindow.ui
+
